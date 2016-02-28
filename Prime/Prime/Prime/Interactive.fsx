715676a9eaab16b6d8c9e2aafded72754a552131
bryanedds <bryanedds@gmail.com>
2016-02-28 05:38:31 -0500

    Removed FSharpx.Core dependency. Removed added lens operators.

--- Prime/Prime/Prime/Interactive.fsx
+++ Prime/Prime/Prime/Interactive.fsx
@@ -2,14 +2,12 @@
 // Copyright (C) Bryan Edds, 2012-2016.
 
 #I __SOURCE_DIRECTORY__
-#r "../../../Prime/FSharpx.Core/FSharpx.Core.dll"
 #r "../../../Prime/FParsec/FParsecCS.dll" // MUST be referenced BEFORE FParsec.dll!
 #r "../../../Prime/FParsec/FParsec.dll"
 #r "../../../Prime/xUnit/xunit.dll"
 #r "../../../Prime/Prime/Prime/bin/Debug/Prime.exe"
 
 open System
-open FSharpx
 open FParsec
 open Prime
 
